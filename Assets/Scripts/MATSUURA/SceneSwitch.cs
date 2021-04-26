00035582533496a4cc3c69bae01f83d44bc15a49
MatsuuraKousei <60458111+HIMAZINX@users.noreply.github.com>
2021-04-26 11:48:48 +0900

    タイトル実装

---
+++ Assets/Scripts/MATSUURA/SceneSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("StageOne");
+        }
+    }
+
+  
+}
---
+++ Assets/Scripts/MATSUURA/TourasRotation.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourasRotation : MonoBehaviour
+{
+    enum Rotations
+    {
+        plusX,
+        minusX,
+        plusY,
+        minusY,
+    }
+
+    private Rotations rotation;
+
+    public int RotationNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (RotationNumber)
+        {
+            case 0:
+                rotation = Rotations.plusX;
+                break;
+            case 1:
+                rotation = Rotations.minusX;
+                break;
+            case 2:
+                rotation = Rotations.plusY;
+                break;
+            case 3:
+                rotation = Rotations.minusY;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (rotation)
+        {
+            case Rotations.plusX:
+                transform.Rotate(new Vector3(0.02f, 0, 0));
+                break;
+            case Rotations.minusX:
+                transform.Rotate(new Vector3(-0.02f, 0, 0));
+                break;
+            case Rotations.plusY:
+                transform.Rotate(new Vector3(0, 0.02f, 0));
+                break;
+            case Rotations.minusY:
+                transform.Rotate(new Vector3(0, -0.02f, 0));
+                break;
+            default:
+                transform.Rotate(new Vector3(0, 0.01f, 0));
+                break;
+        }
+
+    }
+}
