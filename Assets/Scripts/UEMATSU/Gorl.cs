2e662b1139d493011ed694920b568969760b5ffe
Matti <60887233+Matti@users.noreply.github.com>
2021-05-04 15:10:25 +0900

    [植松処理追加]スコア画面で時間と何回トライしたかの回数を表示する処理を追加

--- Assets/Scripts/UEMATSU/Gorl.cs
+++ Assets/Scripts/UEMATSU/Gorl.cs
@@ -6,6 +6,13 @@ public class Gorl : MonoBehaviour
 {
     //クリアパネルを追加する
     public GameObject ClearCanvas;
+    //スコアを管理するオブジェクトを追加
+    public GameObject ScoreManager;
+
+    private void Start()
+    {
+        ScoreManager = GameObject.Find("ScoreManeger");
+    }
 
     //プレイヤーが当たり判定に入った時の処理
     private void OnTriggerEnter(Collider other)
@@ -13,6 +20,8 @@ public class Gorl : MonoBehaviour
         if(other.gameObject.tag=="Player")
         {
             ClearCanvas.SetActive(true);
+            ScoreManager.GetComponent<ScoreManeger>().StopTime(true);
+            ScoreManager.GetComponent<ScoreManeger>().DisplayScore();
         }
     }
 }
---
+++ Assets/Scripts/UEMATSU/ScoreManeger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManeger : MonoBehaviour
+{
+    //かかった時間を表示するテキスト
+    public Text TimeText;
+    private float time = 0;
+    //メビウスシステムを使った回数を表示するテキスト
+    public Text TryText = null;
+    private int TryNum = 0;
+
+    //時間のカウントを止めるための変数
+    private bool FreezTime = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //時間と挑戦回数のリセット
+        time = 0;
+        TryNum = 0;
+        FreezTime = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間を進めないときはここで帰る
+        if (FreezTime) { return; }
+
+        //時間を進める
+        time += Time.deltaTime;
+    }
+
+    public void StopTime(bool isStop)
+    {
+        //時間のカウントを止める
+        FreezTime = isStop;
+    }
+
+    public void AddTryNum()
+    {
+        TryNum += 1;
+    }
+
+    public void DisplayScore()
+    {
+        //時間と挑戦の回数をUIのテキストに表示する
+        TimeText.text = time.ToString("N0")+"Count";
+        TryText.text = TryNum.ToString("N0")+"Num";
+    }
+}
