755926a464db402a03f0aa3aae570a5f5ee32c75
Matti <60887233+Matti@users.noreply.github.com>
2021-05-28 14:18:39 +0900

    [植松処理追加]ゲーム終了ボタンを実装

---
+++ Assets/Scripts/UEMATSU/GameEnd.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        //ƒQ[ƒ€‚ğI—¹‚³‚¹‚éˆ—
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void OnClickStartButton()
+    {
+        Quit();
+    }
+}
